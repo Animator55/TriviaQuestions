253c9d913807044d6e748ceace518f1e620be49b
Animator55 <animator55developer@gmail.com>
2025-02-25 19:44:22 -0300

    added bases

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react"
+import TriviaGame from "./components/TriviaGame"
+import Menu from "./components/Menu"
+import { categoryType } from "./vite-env"
+import { faQuestion } from "@fortawesome/free-solid-svg-icons"
 
-function App() {
-  const [count, setCount] = useState(0)
+import "./App.css"
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+export default function App() {
+  const [page, setPage] = React.useState("menu")
+  const [difficulty, setDiff] = React.useState("easy")
+  const [category, setCat] = React.useState<categoryType>(
+    { name: "All Categories", icon: faQuestion, index: undefined },
   )
-}
 
-export default App
+  return <main>
+    {page === "menu" ?
+      <Menu
+        start={() => { setPage("game") }}
+        difficulty={difficulty}
+        setDiff={setDiff}
+        category={category}
+        setCat={setCat}
+      />
+      :
+      <TriviaGame 
+        difficulty={difficulty}
+        category={category}
+        backToMenu={()=>{setPage("menu")}}
+      />
+    }
+  </main>
+}
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="vite/client" />
+
+import { IconDefinition } from "@fortawesome/free-solid-svg-icons"
+
+
+export type questionType = {
+    category: string
+    correct_answer: string
+    difficulty: string
+    incorrect_answers: string[]
+    question: string
+    type: string
+}
+
+export type categoryType = { name: string, icon: IconDefinition, index: undefined | number }
